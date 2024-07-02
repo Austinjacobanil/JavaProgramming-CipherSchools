d89467b5793389ce21d3f8f9f9b937365c56cd8a
Austin Jacob Anil <119104242+Austinjacobanil@users.noreply.github.com>
2024-07-02 20:30:46 +0530

    Fifth commit

---
+++ Lecture9_ImplementationOfObjectsAndInheritance.java
@@ -0,0 +1,18 @@
+class Shape{
+    double pi = 3.14;
+    double  getSquare(int r){
+        return r*r;
+    }
+}
+class Circle extends Shape{
+    void area(int r){
+        System.out.println(pi*getSquare(r));
+    }
+
+}
+public class Lecture9_ImplementationOfObjectsAndInheritance{
+public static void main(String[] args){
+    Circle c = new Circle();
+    c.area(5);
+}
+}
